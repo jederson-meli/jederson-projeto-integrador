625a2463f616dd05d58fe25de7580ba7680fe6d8
Jederson Carvalho Macedo <jederson.macedo@mercadolivre.com>
2022-05-05 18:38:52 -0300

    test: add integration tests at ServiceController

--- src/main/java/br/com/meli/projetointegrador/controller/SectionController.java
+++ src/main/java/br/com/meli/projetointegrador/controller/SectionController.java
@@ -6,6 +6,7 @@ import br.com.meli.projetointegrador.service.WarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,21 +22,25 @@ public class SectionController {
     private WarehouseService warehouseService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_STOCK_MANAGER')")
     public ResponseEntity<SectionPostDTO> postSection(@RequestBody SectionPostDTO sectionPostDTO) {
         return new ResponseEntity<>(SectionPostDTO.map(sectionService.save(SectionPostDTO.map(sectionPostDTO, warehouseService))), HttpStatus.CREATED);
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_STOCK_MANAGER')")
     public ResponseEntity<List<SectionPostDTO>> findAllSections() {
         return new ResponseEntity<>(SectionPostDTO.map(sectionService.findAll()), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_STOCK_MANAGER')")
     public ResponseEntity<SectionPostDTO> findById(@PathVariable Long id) {
         return new ResponseEntity<>(SectionPostDTO.map(sectionService.findById(id)), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_STOCK_MANAGER')")
     public ResponseEntity<SectionPostDTO> updateSection(@PathVariable Long id, @RequestBody SectionPostDTO sectionPostDTO) {
         return new ResponseEntity<>(sectionService.updateSection(id, sectionPostDTO), HttpStatus.CREATED);
     }
